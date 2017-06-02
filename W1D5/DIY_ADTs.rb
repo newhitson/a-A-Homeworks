9794a4a74eeda040e11fb551bdbb8ffd94903cc7
Nicholas Whitson <newhitson@gmail.com>
2017-06-01 23:45:17 -0700

    add a queue

--- W1D5/DIY_ADTs.rb
+++ W1D5/DIY_ADTs.rb
@@ -1,3 +1,24 @@
+class Queue
+  attr_reader :queue
+
+  def initialize(queue = [])
+    @queue = queue
+  end
+
+  def enqueue(el)
+    @queue << el
+  end
+
+  def dequeue
+    @queue.shift
+  end
+
+  def show
+    @queue.dup
+  end
+
+end
+
 class Stack
   attr_reader :stack
 
